84060989747ed0b5bea78ee0bbd6f9eb8119fcdf
Aditya Gupta <slimshady9852@gmail.com>
2020-08-10 21:28:09 +0530

    first commit

--- app/src/main/java/com/example/thegoldenratio/faceRecognition.java
+++ app/src/main/java/com/example/thegoldenratio/faceRecognition.java
@@ -1,27 +1,34 @@
 package com.example.thegoldenratio;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.content.Loader;
 
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 
 import com.squareup.okhttp.MediaType;
 import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Protocol;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
 
-import java.net.URI;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Arrays;
 
 public class faceRecognition extends AppCompatActivity {
 
-    private static final String subscriptionKey = "<Subscription Key>";
+    private static final String subscriptionKey = "1bbcc1273dc742fa9bbc495dd2259cbb";
 
     private static final String uriBase =
-            "https://<My Endpoint String>.com/face/v1.0/detect";
+            "https://goldenratioonface.cognitiveservices.azure.com/face/v1.0/detect?returnFaceId=true&returnFaceLandmarks=false&returnFaceAttributes=age%2Cgender%2CheadPose%2Csmile%2CfacialHair%2Cglasses%2Cemotion%2Chair%2Cmakeup%2Cocclusion%2Caccessories%2Cblur%2Cexposure%2Cnoise";
+
 
     private static final String imageWithFaces =
-            "{\"url\":\"https://upload.wikimedia.org/wikipedia/commons/c/c3/RH_Louise_Lillian_Gish.jpg\"}";
+            "{\"url\":\"https://upload.wikimedia.org/wikipedia/commons/3/37/Dagestani_man_and_woman.jpg\"}";
 
     private static final String faceAttributes =
             "age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise";
@@ -30,20 +37,41 @@ public class faceRecognition extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_face_recognition);
-        OkHttpClient client = new OkHttpClient();
+        final OkHttpClient client = new OkHttpClient();
 
-        Uri.Builder builder = new Uri.Builder();
-        builder.appendEncodedPath(uriBase);
-        builder.appendQueryParameter("returnFaceId", "true");
-        builder.appendQueryParameter("returnFaceLandmarks", "false");
-        builder.appendQueryParameter("returnFaceAttributes", faceAttributes);
-        Uri uri = builder.build();
-        MediaType mediaType = MediaType.parse("application/json");
-        Request request = new Request.Builder()
-                .url(String.valueOf(uri))
-                .addHeader("Content-Type", "application/json")
-                .addHeader("Ocp-Apim-Subscription-Key", subscriptionKey)
-                .build();
+        AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... voids) {
+
+                URL url = null;
+                try {
+                    url = new URL(uriBase);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+
+
+                MediaType mediaType = MediaType.parse("application/json");
+                RequestBody body = RequestBody.create(mediaType,
+                        imageWithFaces);
+                Request request = new Request.Builder()
+                        .url(url)
+                        .addHeader("Ocp-Apim-Subscription-Key", subscriptionKey)
+                        .build();
+                try {
+
+                    Response response = client.newCall(request).execute();
+                    String reu = response.body().string();
+                    Log.i("result", reu);
+                } catch (Exception e) {
+                    Log.i("result", String.valueOf(e));
+
+                }
+                return null;
+            }
+        }.execute();
+
+
 
 
 
