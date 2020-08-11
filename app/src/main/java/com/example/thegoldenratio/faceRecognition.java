0470fcf93a770dd7977fe773ebd7467605ca57ef
Aditya Gupta <slimshady9852@gmail.com>
2020-08-11 23:21:14 +0530

    added goldenRect t-2

--- app/src/main/java/com/example/thegoldenratio/faceRecognition.java
+++ app/src/main/java/com/example/thegoldenratio/faceRecognition.java
@@ -3,10 +3,18 @@ package com.example.thegoldenratio;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.loader.content.Loader;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 
 import com.squareup.okhttp.MediaType;
 import com.squareup.okhttp.OkHttpClient;
@@ -28,11 +36,15 @@ public class faceRecognition extends AppCompatActivity {
 
 
     private static final String imageWithFaces =
-            "{\"url\":\"https://upload.wikimedia.org/wikipedia/commons/3/37/Dagestani_man_and_woman.jpg\"}";
+            "{\"url\":\"https://s.yimg.com/ny/api/res/1.2/2cQ_zadvzJfQ4BJxTzptww--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9NzQ0O2g9OTUw/http://media.zenfs.com/en/homerun/feed_manager_auto_publish_494/cae8ffc764d9c3d6fec857e1344aab8e\"}";
 
     private static final String faceAttributes =
             "age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise";
 
+    private static final String TAG = "TextGraphic";
+    private static final int TEXT_COLOR = Color.RED;
+    private static final float TEXT_SIZE = 54.0f;
+    private static final float STROKE_WIDTH = 4.0f;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -74,6 +86,31 @@ public class faceRecognition extends AppCompatActivity {
                 return null;
             }
         }.execute();
+
+        ImageView myImageView = findViewById(R.id.imageView);
+        Bitmap myBitmap = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.monalisa);
+        Paint myRectPaint;
+        myRectPaint = new Paint();
+        myRectPaint.setColor(TEXT_COLOR);
+        myRectPaint.setStyle(Paint.Style.STROKE);
+        myRectPaint.setStrokeWidth(STROKE_WIDTH);
+        int x1 = 55;
+        int y1 = 55;
+        int x2 = 1500;
+        int y2 = 900;
+
+//Create a new image bitmap and attach a brand new canvas to it
+        Bitmap tempBitmap = Bitmap.createBitmap(myBitmap.getWidth(), myBitmap.getHeight(), Bitmap.Config.RGB_565);
+        Canvas tempCanvas = new Canvas(tempBitmap);
+
+//Draw the image bitmap into the cavas
+        tempCanvas.drawBitmap(myBitmap, 0, 0, null);
+
+//Draw everything else you want into the canvas, in this example a rectangle with rounded edges
+        tempCanvas.drawRoundRect(new RectF(x1,y1,x2,y2), 2, 2, myRectPaint);
+
+//Attach the canvas to the ImageView
+        myImageView.setImageDrawable(new BitmapDrawable(getResources(), tempBitmap));
 
 
 
