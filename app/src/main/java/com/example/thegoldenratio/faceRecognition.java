4a6203f384facea47a572f05dfe44d9987e32038
Aditya Gupta <slimshady9852@gmail.com>
2020-08-12 19:13:29 +0530

    added ratio on face

--- app/src/main/java/com/example/thegoldenratio/faceRecognition.java
+++ app/src/main/java/com/example/thegoldenratio/faceRecognition.java
@@ -1,8 +1,6 @@
 package com.example.thegoldenratio;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.loader.content.Loader;
-
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
@@ -10,12 +8,15 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.graphics.drawable.BitmapDrawable;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.TypedValue;
 import android.widget.ImageView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.common.io.ByteStreams;
 import com.squareup.okhttp.MediaType;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Protocol;
@@ -23,6 +24,12 @@ import com.squareup.okhttp.Request;
 import com.squareup.okhttp.RequestBody;
 import com.squareup.okhttp.Response;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Arrays;
@@ -36,7 +43,7 @@ public class faceRecognition extends AppCompatActivity {
 
 
     private static final String imageWithFaces =
-            "{\"url\":\"https://s.yimg.com/ny/api/res/1.2/2cQ_zadvzJfQ4BJxTzptww--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9NzQ0O2g9OTUw/http://media.zenfs.com/en/homerun/feed_manager_auto_publish_494/cae8ffc764d9c3d6fec857e1344aab8e\"}";
+            "{\"url\":\"https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/elm120117wlcoverstory-011copy-1509935070.jpg\"}";
 
     private static final String faceAttributes =
             "age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise";
@@ -51,9 +58,11 @@ public class faceRecognition extends AppCompatActivity {
         setContentView(R.layout.activity_face_recognition);
         final OkHttpClient client = new OkHttpClient();
 
-        AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>() {
+
+
+        AsyncTask<Void, Void, String> task = new AsyncTask<Void, Void, String>() {
             @Override
-            protected Void doInBackground(Void... voids) {
+            protected String doInBackground(Void... voids) {
 
                 URL url = null;
                 try {
@@ -61,60 +70,101 @@ public class faceRecognition extends AppCompatActivity {
                 } catch (MalformedURLException e) {
                     e.printStackTrace();
                 }
-
-
-                MediaType mediaType = MediaType.parse("application/json");
-                RequestBody body = RequestBody.create(mediaType,
-                        imageWithFaces);
-                Request request = new Request.Builder()
-                        .url(uriBase).post(body)
-                        .addHeader("Ocp-Apim-Subscription-Key", subscriptionKey)
-                        .addHeader("Ocp-Apim-Subscription-Region", "centralindia")
-                        .addHeader("Content-type", "application/json")
-                        .build();
+                ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                Bitmap myBitmap = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.jennif);
+                myBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+                ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
+                byte[] localImageBytes;
                 try {
+                    localImageBytes = ByteStreams.toByteArray(inputStream);
+                    MediaType mediaType = MediaType.parse("application/octet-stream");
+                    RequestBody body = RequestBody.create(mediaType,
+                            localImageBytes);
+                    Request request = new Request.Builder()
+                            .url(uriBase).post(body)
+                            .addHeader("Ocp-Apim-Subscription-Key", subscriptionKey)
+                            .addHeader("Ocp-Apim-Subscription-Region", "centralindia")
+                            .addHeader("Content-type", "application/json")
+                            .build();
                     client.setProtocols(Arrays.asList(Protocol.HTTP_1_1));
 
 
                     Response response = client.newCall(request).execute();
                     String reu = response.body().string();
                     Log.i("result", reu);
-                } catch (Exception e) {
-                    Log.i("result", String.valueOf(e));
-
+                    return reu;
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-                return null;
+
+                return "";
             }
-        }.execute();
+        };
+        String result ="";
+        try {
+            result = task.execute().get();
+        }catch (Exception e){
+            Log.i("result", e.toString());
+        }
+        try {
+            JSONArray output = new JSONArray(result);
+            JSONObject one = output.getJSONObject(0);
+            JSONObject faceRectangle = one.getJSONObject("faceRectangle");
+            double top = faceRectangle.getDouble("top");
+            double left = faceRectangle.getDouble("left");
+            double width = faceRectangle.getDouble("width");
+            double height = faceRectangle.getDouble("height");
 
-        ImageView myImageView = findViewById(R.id.imageView);
-        Bitmap myBitmap = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.monalisa);
-        Paint myRectPaint;
-        myRectPaint = new Paint();
-        myRectPaint.setColor(TEXT_COLOR);
-        myRectPaint.setStyle(Paint.Style.STROKE);
-        myRectPaint.setStrokeWidth(STROKE_WIDTH);
-        int x1 = 55;
-        int y1 = 55;
-        int x2 = 1500;
-        int y2 = 900;
+            JSONObject faceLandmarks = one.getJSONObject("faceLandmarks");
+            JSONObject noseTip = faceLandmarks.getJSONObject("noseTip");
+            double noseTipX = noseTip.getDouble("x");
+            double noseTipY = noseTip.getDouble("y");
+            JSONObject eyeRightInner = faceLandmarks.getJSONObject("eyeRightInner");
+            double eyeRightInnerX = eyeRightInner.getDouble("x");
+            double eyeRightInnerY = eyeRightInner.getDouble("y");
+            JSONObject eyeRightOuter = faceLandmarks.getJSONObject("eyeRightOuter");
+            double eyeRightOuterX = eyeRightOuter.getDouble("x");
+            double eyeRightOuterY = eyeRightOuter.getDouble("y");
 
-//Create a new image bitmap and attach a brand new canvas to it
-        Bitmap tempBitmap = Bitmap.createBitmap(myBitmap.getWidth(), myBitmap.getHeight(), Bitmap.Config.RGB_565);
-        Canvas tempCanvas = new Canvas(tempBitmap);
+            ImageView myImageView = findViewById(R.id.imageView);
+            Bitmap myBitmap = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.jennif);
+            Paint myRectPaint;
+            myRectPaint = new Paint();
+            myRectPaint.setColor(TEXT_COLOR);
+            myRectPaint.setStyle(Paint.Style.STROKE);
+            myRectPaint.setStrokeWidth(STROKE_WIDTH);
+            int x1 = (int)noseTipX;
+            int y1 = (int)eyeRightOuterY;
+            int x2 = (int)eyeRightOuterX;
+            int y2 = (int)noseTipY;
+            Log.i("result", x1 + " " + y1 + " " + x2 + " " + y2);
 
-//Draw the image bitmap into the cavas
-        tempCanvas.drawBitmap(myBitmap, 0, 0, null);
+            Bitmap tempBitmap = Bitmap.createBitmap(myBitmap.getWidth(), myBitmap.getHeight(), Bitmap.Config.RGB_565);
+            Log.i("result", myBitmap.getWidth() + " "+ myBitmap.getHeight());
+            Canvas tempCanvas = new Canvas(tempBitmap);
 
-//Draw everything else you want into the canvas, in this example a rectangle with rounded edges
-        tempCanvas.drawRoundRect(new RectF(x1,y1,x2,y2), 2, 2, myRectPaint);
+            tempCanvas.drawBitmap(myBitmap, 0, 0, null);
 
-//Attach the canvas to the ImageView
-        myImageView.setImageDrawable(new BitmapDrawable(getResources(), tempBitmap));
+            tempCanvas.drawRoundRect(new RectF(x1,y1,x2,y2), 2, 2, myRectPaint);
+
+            myImageView.setImageDrawable(new BitmapDrawable(getResources(), tempBitmap));
+        }catch (Exception e){
+            Log.i("result", e.toString());
+        }
 
 
 
 
 
+
+
+
+    }
+    public static float px2dp(Resources resource, float px)  {
+        return (float) TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_PX,
+                px,
+                resource.getDisplayMetrics()
+        );
     }
 }
