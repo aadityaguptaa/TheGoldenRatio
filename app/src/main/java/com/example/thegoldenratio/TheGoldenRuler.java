fe2336b17675fd8d43fd4d8822b076b7f1502031
Aditya Gupta <slimshady9852@gmail.com>
2020-08-25 12:55:19 +0530

    ruler t-2

--- app/src/main/java/com/example/thegoldenratio/TheGoldenRuler.java
+++ app/src/main/java/com/example/thegoldenratio/TheGoldenRuler.java
@@ -1,209 +1,74 @@
 package com.example.thegoldenratio;
 
-import android.content.DialogInterface;
+import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Camera;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Surface;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
-import android.view.WindowManager;
-import android.widget.Button;
+import android.widget.ImageView;
 
-import androidx.appcompat.app.AlertDialog;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
+import java.io.File;
 import java.io.IOException;
-import java.util.List;
 
-public class TheGoldenRuler extends AppCompatActivity implements SurfaceHolder.Callback {
-    private SurfaceView surfaceView;
-    private SurfaceHolder surfaceHolder;
-    private Camera camera;
-    private Button flipCamera;
-    private Button flashCameraButton;
-    private Button captureImage;
-    private int cameraId;
-    private boolean flashmode = false;
-    private int rotation;
+public class TheGoldenRuler extends AppCompatActivity {
 
+    public String currentPhotoPath;
+    public Bitmap mSelectedImage;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == 1) {
+            final Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath);
+            if (bitmap != null) {
+                final ImageView im = findViewById(R.id.rulerCameraView);
+                im.setVisibility(View.VISIBLE);
+                mSelectedImage = Bitmap.createScaledBitmap(bitmap, im.getWidth(), im.getHeight(), true);
+                im.setImageBitmap(mSelectedImage);
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_the_golden_ruler);
-        cameraId = android.hardware.Camera.CameraInfo.CAMERA_FACING_BACK;
-        flipCamera = (Button) findViewById(R.id.flipCamera);
-        flashCameraButton = (Button) findViewById(R.id.flash);
-        captureImage = (Button) findViewById(R.id.captureImage);
-        surfaceView = (SurfaceView) findViewById(R.id.surfaceView);
-        surfaceHolder = surfaceView.getHolder();
-        surfaceHolder.addCallback(this);
-        flipCamera.setOnClickListener(this);
-        captureImage.setOnClickListener(this);
-        flashCameraButton.setOnClickListener(this);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
-        if (!getBaseContext().getPackageManager().hasSystemFeature(
-                PackageManager.FEATURE_CAMERA_FLASH)) {
-            flashCameraButton.setVisibility(View.GONE);
+
+        if(ContextCompat.checkSelfPermission(TheGoldenRuler.this, Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(TheGoldenRuler.this, new String[]{
+                    Manifest.permission.CAMERA
+            }, 100);
         }
-    }
-
-    @Override
-    public void surfaceCreated(SurfaceHolder holder) {
-        if (!openCamera(android.hardware.Camera.CameraInfo.CAMERA_FACING_BACK)) {
-            alertCameraDialog ();
-        }
-
-    }
-
-    @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-
-    }
-
-    @Override
-    public void surfaceDestroyed(SurfaceHolder holder) {
-
-    }
-
-    private void alertCameraDialog() {
-        AlertDialog.Builder dialog = createAlert(TheGoldenRuler.this,
-                "Camera info", "error to open camera");
-        dialog.setNegativeButton("OK", new DialogInterface.OnClickListener() {
+        ImageView cameraClick = findViewById(R.id.rulerFloatButton);
+        cameraClick.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.cancel();
-
+            public void onClick(View v) {
+                String fileName = "photo";
+                File storageDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+                try{
+                    File imageFile = File.createTempFile(fileName, ".jpg", storageDirectory);
+                    currentPhotoPath = imageFile.getAbsolutePath();
+                    Uri imageUri = FileProvider.getUriForFile(TheGoldenRuler.this, "com.example.thegoldenratio.fileprovider", imageFile);
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                    startActivityForResult(intent, 1);
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
             }
         });
-
-        dialog.show();
-    }
-
-    private boolean openCamera(int id) {
-        boolean result = false;
-        cameraId = id;
-        releaseCamera();
-        try {
-            camera = Camera.open(cameraId);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        if (camera != null) {
-            try {
-                setUpCamera(camera);
-                camera.setErrorCallback(new ErrorCallback() {
-
-                    @Override
-                    public void onError(int error, Camera camera) {
-//to show the error message.
-                    }
-                });
-                camera.setPreviewDisplay(surfaceHolder);
-                camera.startPreview();
-                result = true;
-            } catch (IOException e) {
-                e.printStackTrace();
-                result = false;
-                releaseCamera();
-            }
-        }
-        return result;
-    }
-
-    private void releaseCamera() {
-        try {
-            if (camera != null) {
-                camera.setPreviewCallback(null);
-                camera.setErrorCallback(null);
-                camera.stopPreview();
-                camera.release();
-                camera = null;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            Log.e("error", e.toString());
-            camera = null;
-        }
-    }
-
-    private void setUpCamera(Camera c) {
-        Camera.CameraInfo info = new Camera.CameraInfo();
-        Camera.getCameraInfo(cameraId, info);
-        rotation = getWindowManager().getDefaultDisplay().getRotation();
-        int degree = 0;
-        switch (rotation) {
-            case Surface.ROTATION_0:
-                degree = 0;
-                break;
-            case Surface.ROTATION_90:
-                degree = 90;
-                break;
-            case Surface.ROTATION_180:
-                degree = 180;
-                break;
-            case Surface.ROTATION_270:
-                degree = 270;
-                break;
-
-            default:
-                break;
-        }
-
-        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
-            // frontFacing
-            rotation = (info.orientation + degree) % 330;
-            rotation = (360 - rotation) % 360;
-        } else {
-            // Back-facing
-            rotation = (info.orientation - degree + 360) % 360;
-        }
-        c.setDisplayOrientation(rotation);
-        Parameters params = c.getParameters();
-
-        showFlashButton(params);
-
-        List<String> focusModes = params.getSupportedFlashModes();
-        if (focusModes != null) {
-            if (focusModes
-                    .contains(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE)) {
-                params.setFlashMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
-            }
-        }
-
-        params.setRotation(rotation);
-    }
-
-    private void showFlashButton(Parameters params) {
-        boolean showFlash = (getPackageManager().hasSystemFeature(
-                PackageManager.FEATURE_CAMERA_FLASH) && params.getFlashMode() != null)
-                && params.getSupportedFlashModes() != null
-                && params.getSupportedFocusModes().size() > 1;
-
-        flashCameraButton.setVisibility(showFlash ? View.VISIBLE
-                : View.INVISIBLE);
-
-    }
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.flash:
-                flashOnButton();
-                break;
-            case R.id.flipCamera:
-                flipCamera();
-                break;
-            case R.id.captureImage:
-                takeImage();
-                break;
-
-            default:
-                break;
-        }
     }
 
 }
