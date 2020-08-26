67c50fc915e1b454ad569a5c003b7f78322267df
Aditya Gupta <slimshady9852@gmail.com>
2020-08-26 22:04:59 +0530

    added description in Earth and Moon

--- app/src/main/java/com/example/thegoldenratio/EarthAndMoon.java
+++ app/src/main/java/com/example/thegoldenratio/EarthAndMoon.java
@@ -15,5 +15,36 @@ public class EarthAndMoon extends AppCompatActivity {
 
         GifImageView gifImageView = findViewById(R.id.earthGif);
 
+        final String[] st = new String[4];
+        st[0] = "Did you know??";
+        st[1] = "The dimensions of the Earth and Moon are in Phi relationship, forming a Triangle based on 1.618.";
+        st[2] = "Don't believe me??";
+        st[3] = "Lemme show you!!";
+
+        //Add a character every 150ms
+         // Delay in seconds
+        final TypeWriter writer = findViewById(R.id.typeWriter);
+        writer.setCharacterDelay(75);
+        writer.animateText(st[0]);
+
+        Utils.delay(3, new Utils.DelayCallback() {
+            @Override
+            public void afterDelay() {
+                writer.animateText(st[1]);
+                Utils.delay(11, new Utils.DelayCallback() {
+                    @Override
+                    public void afterDelay() {
+                        writer.animateText(st[2]);
+                        Utils.delay(3, new Utils.DelayCallback() {
+                            @Override
+                            public void afterDelay() {
+                                writer.animateText(st[3]);
+                            }
+                        });
+                    }
+                });
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/thegoldenratio/Utils.java
@@ -0,0 +1,23 @@
+package com.example.thegoldenratio;
+
+import android.os.Handler;
+
+
+public class Utils {
+
+    // Delay mechanism
+
+    public interface DelayCallback{
+        void afterDelay();
+    }
+
+    public static void delay(int secs, final DelayCallback delayCallback){
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                delayCallback.afterDelay();
+            }
+        }, secs * 1000); // afterDelay will be executed after (secs*1000) milliseconds.
+    }
+}
