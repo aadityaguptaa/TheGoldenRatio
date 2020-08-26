69219720536f57dc1c252116b08692546d22dad7
Aditya Gupta <slimshady9852@gmail.com>
2020-08-26 23:48:55 +0530

    Earth and moon t-4

--- app/src/main/java/com/example/thegoldenratio/EarthAndMoon.java
+++ app/src/main/java/com/example/thegoldenratio/EarthAndMoon.java
@@ -1,20 +1,35 @@
 package com.example.thegoldenratio;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import pl.droidsonroids.gif.GifImageView;
 
 public class EarthAndMoon extends AppCompatActivity {
+    private int verticalsButtonsCount = 0;
+    ImageView verticalLine;
+    ImageView horizontalLine;
+    GifImageView earthView;
+    GifImageView moonView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_earth_and_moon);
+        verticalLine = findViewById(R.id.verticalLine);
+        horizontalLine = findViewById(R.id.horizontalLine);
+        earthView = findViewById(R.id.earthGif);
+        moonView = findViewById(R.id.moonGif);
 
-        GifImageView gifImageView = findViewById(R.id.earthGif);
+        float earthViewHeight = earthView.getHeight();
+        float earthViewWidth = earthView.getWidth();
+        float moonViewHeight = moonView.getHeight();
 
+        verticalLine.getLayoutParams().height = (int) ((earthViewHeight/2) + (moonViewHeight/2));
+        horizontalLine.getLayoutParams().width = (int)(earthViewWidth/2);
         final String[] st = new String[4];
         st[0] = "Did you know??";
         st[1] = "The dimensions of the Earth and Moon are in Phi relationship, forming a Triangle based on 1.618.";
@@ -46,5 +61,13 @@ public class EarthAndMoon extends AppCompatActivity {
             }
         });
 
+    }
+    public void onClickHorizontal(View view){
+        verticalsButtonsCount+=1;
+        if(verticalsButtonsCount == 2){
+            verticalLine.setVisibility(View.VISIBLE);
+        }else if(verticalsButtonsCount == 3){
+            horizontalLine.setVisibility(View.VISIBLE);
+        }
     }
 }
