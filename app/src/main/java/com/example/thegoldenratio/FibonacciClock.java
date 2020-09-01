2a17ecf61a2a99c27d54b65fcb5b7decf3e57ad9
Aditya Gupta <slimshady9852@gmail.com>
2020-09-01 14:38:31 +0530

    fibonacci clock first commit

---
+++ app/src/main/java/com/example/thegoldenratio/FibonacciClock.java
@@ -0,0 +1,14 @@
+package com.example.thegoldenratio;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FibonacciClock extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fibonacci_clock);
+    }
+}
